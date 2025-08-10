5953c35ef4c3b1ec82cde4b559824b00a57f5a18
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-08-10 10:49:04 +1000

    Apply Modifications to flaskdemo

--- flaskdemo.py
+++ flaskdemo.py
@@ -19,7 +19,7 @@ def home():
 @app.route('/about')
 def about():
     """About page route."""
-    return "I am still working on this"
+    return render_template("about.html")
 
 
 @app.route('/search', methods=['POST', 'GET'])
@@ -37,6 +37,7 @@ def results():
     search_term = session['search_term']
     page = get_page(search_term)
     return render_template("results.html", page=page)
+
 
 
 def get_page(search_term):
